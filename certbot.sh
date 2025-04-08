c3d6e914493c564c1bb90b9b1c3e7133562061c8
Tamás Tóth <tomjtoth@gmail.com>
2025-04-08 16:27:36 +0300

    extracted entrypoint into a script, relocated certificate, adjusted .gitignore

---
+++ certbot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# this script is invoked from within the certbot container
+
+if [ ! -d "/etc/letsencrypt/live/ttj.hu" ]; then
+    certbot certonly \
+        --dns-cloudflare \
+        --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
+        --email tomjtoth@gmail.com \
+        --agree-tos \
+        --non-interactive \
+        -d *.ttj.hu
+fi;
+
+# renewal loop
+trap exit TERM;
+while :; do
+    certbot renew \
+        --dns-cloudflare \
+        --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
+        --post-hook "echo Certificates renewed!";
+
+    sleep 12h & wait $${!};
+done
